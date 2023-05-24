aa001154ea756dbb30468717fdb9131651f34aab
NoahKirchner <noahthomkirchner@gmail.com>
2023-05-24 13:52:21 -0400

    Adds ubuntu server and securityonion standalone to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -141,3 +141,58 @@ resource "proxmox_vm_qemu" "ubuntu_desktop" {
 
 }
 
+resource "proxmox_vm_qemu" "ubuntu_server" {
+    count = 2
+
+    name = "ubuntu-server-tf-${count.index}"
+    target_node = "r730"
+    clone = "UbuntuServer"
+    full_clone = true
+    os_type = "linux"
+    sockets = 2
+    cores = 2
+    memory = "4096"
+    scsihw = "virtio-scsi-pci"
+    oncreate = true 
+
+    disk {
+        size = "50G"
+        type = "scsi"
+        storage = "local"
+    }
+
+    network {
+        model = "virtio"
+        bridge = "vmbr2"
+    }
+
+    bootdisk = "scsi0"
+}
+
+resource "proxmox_vm_qemu" "seconion_standalone" {
+    count = 1
+
+    name = "seconion-standalone-tf-${count.index}"
+    target_node = "r730"
+    clone = "SecurityOnion"
+    full_clone = true 
+    os_type = "linux"
+    sockets = 4
+    cores = 4
+    memory = "32786"
+    scsihw = "virtio-scsi-pci"
+    oncreate = true
+
+    disk {
+        size = "500G"
+        type = "scsi"
+        storage = "local"
+    }
+
+    network {
+        model = "virtio"
+        bridge = "vmbr2"
+    }
+
+    bootdisk = "scsi0"
+}
