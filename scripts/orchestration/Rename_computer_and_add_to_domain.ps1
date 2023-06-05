60b33f442f551e2595498d8e232c337af370244e
dsisk69 <135341616+dsisk69@users.noreply.github.com>
2023-06-05 10:28:14 -0400

    Powershell rename computer and add to domain

---
+++ scripts/orchestration/Rename_computer_and_add_to_domain.ps1
@@ -0,0 +1,29 @@
+﻿#Sets values of characters and numbers to pull from
+$TokenSet = @{
+        U = [Char[]]'DEFENDER'
+        L = [Char[]]'defender'
+        N = [Char[]]'69'
+    }
+
+#Sets values to variables
+$Upper = Get-Random -Count 5 -InputObject $TokenSet.U
+$Lower = Get-Random -Count 5 -InputObject $TokenSet.L
+$Number = Get-Random -Count 5 -InputObject $TokenSet.N
+
+#Assembles variable
+$StringSet = $Upper + $Lower + $Number
+$computername = (Get-Random -Count 15 -InputObject $StringSet) -join ''
+
+#Renames computer
+Rename-Computer $computername
+
+#Sets domain information to variables
+$domain = "purple.mat"
+$password = "1qaz2wsx!QAZ@WSX" | ConvertTo-SecureString -asPlainText -Force
+$username = "purple\Administrator" 
+
+#Automates the input of domain credentials
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+#Adds the computer to domain
+Add-Computer -DomainName $domain -Credential $credential
