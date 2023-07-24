db2dbd4cc0ef2becd8282b9d0f9e2f4847a25201
dsisk69 <135341616+dsisk69@users.noreply.github.com>
2023-07-24 15:19:26 -0400

    Update Rename_computer_and_add_to_domain.ps1
    Updated

--- scripts/orchestration/Rename_computer_and_add_to_domain.ps1
+++ scripts/orchestration/Rename_computer_and_add_to_domain.ps1
@@ -1,43 +1,81 @@
+# Specify the registry key you want to check and create if it doesn't exist
+$registryKeyHOSTNAME = "HKLM:\Software\Purple\Hostname"
 
-Set-ExecutionPolicy Bypass -scope Process -Force
+# Check if the registry key exists
+if (Test-Path -Path $registryKeyHOSTNAME) {
+    # If the key exists, execute your commands here
+    Write-Host "Hostname has already been changed. Computer will now be added to the domain then reboot"
+    # Add your commands here:
+    # For example, you can call other scripts or run any other PowerShell commands.
+}
+else {
+    # If the key doesn't exist, create it and then execute commands
+    Write-Host "Hostname has not been changed yet. Computer will reboot after changes are made"
+    New-Item -Path $registryKeyHOSTNAME -Force | Out-Null
+    # Add any additional registry settings or values you want to create here, if needed.
 
-#Sets values of characters and numbers to pull from
-$TokenSet = @{
-        U = [Char[]]'DEFENDER'
-        L = [Char[]]'defender'
-        N = [Char[]]'69'
-    }
+    # Now, execute your commands here as the key is created.
 
-#Sets values to variables
-$Upper = Get-Random -Count 5 -InputObject $TokenSet.U
-$Lower = Get-Random -Count 5 -InputObject $TokenSet.L
-$Number = Get-Random -Count 5 -InputObject $TokenSet.N
+    #Sets values of characters and numbers to pull from
+    $TokenSet = @{
+            U = [Char[]]'DEFENDER'
+            L = [Char[]]'defender'
+            N = [Char[]]'69'
+        }
 
-#Assembles variable
-$StringSet = $Upper + $Lower + $Number
-$computername = (Get-Random -Count 15 -InputObject $StringSet) -join ''
+    #Sets values to variables
+    $Upper = Get-Random -Count 5 -InputObject $TokenSet.U
+    $Lower = Get-Random -Count 5 -InputObject $TokenSet.L
+    $Number = Get-Random -Count 5 -InputObject $TokenSet.N
 
-#Renames computer
-Rename-Computer $computername
+    #Assembles variable
+    $StringSet = $Upper + $Lower + $Number
+    $computername = (Get-Random -Count 15 -InputObject $StringSet) -join ''
 
-Start-Sleep 5
+    #Renames computer
+    Rename-Computer $computername
+  
+    Start-Sleep 1
 
-#Sets DNS to Domain Controller
 
-$IntIndex = Get-NetAdapter | Select-Object InterfaceIndex
-Set-DnsClientServerAddress -InterfaceIndex $IntIndex.InterfaceIndex -ServerAddresses 192.168.2.2
+    Restart-Computer
 
-#Sets domain information to variables
-$domain = "purple.mat"
-$password = "1qaz2wsx!QAZ@WSX" | ConvertTo-SecureString -asPlainText -Force
-$username = "purple\Administrator" 
+    # Add your commands here.
+}
+# Specify the registry key you want to check and create if it doesn't exist
+$registryKeyDOMAIN = "HKLM:\Software\Purple\Domain"
 
-#Automates the input of domain credentials
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+# Check if the registry key exists
+if (Test-Path -Path $registryKeyDOMAIN) {
+    # If the key exists, execute your commands here
+    Write-Host "This computer has already been added to the domain"
+    # Add your commands here:
+    # For example, you can call other scripts or run any other PowerShell commands.
+}
+else {
+    # If the key doesn't exist, create it and then execute commands
+    Write-Host "Computer is being added to the domain. Computer will reboot after changes are made"
+    New-Item -Path $registryKeyDOMAIN -Force | Out-Null
+    # Add any additional registry settings or values you want to create here, if needed.
 
-#Adds the computer to domain
-Add-Computer -DomainName $domain -Credential $credential
+    # Now, execute your commands here as the key is created.
 
-Start-Sleep 5
+    #Sets DNS to Domain Controller
+    $IntIndex = Get-NetAdapter | Select-Object InterfaceIndex
+    Set-DnsClientServerAddress -InterfaceIndex $IntIndex.InterfaceIndex -ServerAddresses 192.168.2.2
 
-Restart-Computer
+    #Sets domain information to variables
+    $domain = "purple.mat"
+    $password = "1qaz2wsx!QAZ@WSX" | ConvertTo-SecureString -asPlainText -Force
+    $username = "purple\Administrator" 
+
+    #Automates the input of domain credentials
+    $credential = New-Object System.Management.Automation.PSCredential($username,$password)
+
+    #Adds the computer to domain
+    Add-Computer -DomainName $domain -Credential $credential
+
+    Start-Sleep 1
+
+    Restart-Computer
+}
